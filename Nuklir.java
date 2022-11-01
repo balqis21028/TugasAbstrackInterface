fc3bf69812a54f8d6a293dfef3b2f1ed7797874c
balqis21028 <114387479+balqis21028@users.noreply.github.com>
2022-11-01 10:53:30 +0700

    Add files via upload
    Tombol

---
+++ Nuklir.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tombol;
+
+/**
+ *
+ * @author ASUS
+ */
+class Nuklir extends Tombol{
+    @Override
+    public void Tekan() {
+        System.out.println("Bom Nuklir Telah Diluncurkan!");
+        System.out.println("BOOOOMMM!!!");
+    }
+}
